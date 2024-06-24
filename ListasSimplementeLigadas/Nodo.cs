c6065afea05b426d92cb731a3566258cf93ba603
Jose Javier Mendoza Villalpando <Jose_Mendoza23@unikino.edu.mx>
2024-06-24 07:56:19 -0700

    Creando solucion y proyecto listas simplemente ligadas

---
+++ ListasSimplementeLigadas/Nodo.cs
@@ -0,0 +1,8 @@
+﻿namespace ListasSimplementeLigadas
+{
+    internal class Nodo
+    {
+        public string Dato { get; set; } = string.Empty;
+        public Nodo? Enlace { get; set; } = null;
+    }
+}
