b1362b13a9e405e26654e5a2366ab8dc65c8fb20
Jefte Elioenai Zamarron Pule <Jefte_Zamarron23@unikino.edu.mx>
2024-07-08 07:57:36 -0700

    Agregando la clase lista

---
+++ ListasDoblementeLigadas/Nodo.cs
@@ -0,0 +1,9 @@
+﻿namespace ListasDoblementeLigadas
+{
+    internal class Nodo
+    {
+        public Nodo? Anterior { get; set; } = null;
+        public string Dato { get; set; } = string.Empty;
+        public Nodo? Siguiente { get; set; } = null;
+    }
+}
