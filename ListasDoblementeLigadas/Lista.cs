f86b65d2add8065142dbc1d442962d98a1f8a625
Jose Javier Mendoza Villalpando <Jose_Mendoza23@unikino.edu.mx>
2024-07-11 07:49:14 -0700

    agregamos metodos de busqueda y eliminar

--- ListasDoblementeLigadas/Lista.cs
+++ ListasDoblementeLigadas/Lista.cs
@@ -7,10 +7,10 @@
         {
             _nodoInicial = new Nodo();
         }
-
+        //Metodo para validar si la lista esta vacia
         private bool EstaVacio()
         {
-            if (_nodoInicial.Siguiente == null) 
+            if (_nodoInicial.Siguiente == null)
             {
                 return true;
             }
@@ -19,17 +19,17 @@
                 return false;
             }
         }
-
+        //Mertodo para vaciar la lista
         public void Vaciar()
         {
             _nodoInicial.Siguiente = null;
         }
-
+        //Metodo de para agregar nodos
         public void Agregar(string dato)
         {
             Nodo nodoActual = _nodoInicial;
-            while (nodoActual.Siguiente != null) 
-            { 
+            while (nodoActual.Siguiente != null)
+            {
                 nodoActual = nodoActual.Siguiente;
             }
 
@@ -38,6 +38,42 @@
             nodoNuevo.Dato = dato;
 
             nodoActual.Siguiente = nodoNuevo;
+        }
+        //metodo de busqueda de nodos
+        public Nodo? Buscar(string dato)
+        {
+            if (EstaVacio() == false)
+            {
+                Nodo nodoActual = _nodoInicial;
+                while (nodoActual.Siguiente != null)
+                {
+                    nodoActual = nodoActual.Siguiente;
+                    if (nodoActual.Dato == dato)
+                    {
+                        return nodoActual;
+                    }
+                }
+            }
+            return null;
+        }
+        //Metodo para eliminar nodos
+        public void Eliminar(string dato)
+        {
+            if (EstaVacio() == false)
+            {
+                Nodo? nodoActual = Buscar(dato);
+                //metodo para navegar en las propiedades de los nodos
+                if (nodoActual != null)
+                {
+                    //Reubicando los enlaces
+                    nodoActual.Anterior!.Siguiente = nodoActual.Siguiente;
+                    nodoActual.Siguiente!.Anterior = nodoActual.Anterior;
+
+                    //opcionalmente eliminamos los enlaces del nodo a eliminar
+                    nodoActual.Anterior = null;
+                    nodoActual.Siguiente = null;
+                }
+            }
         }
     }
 }
