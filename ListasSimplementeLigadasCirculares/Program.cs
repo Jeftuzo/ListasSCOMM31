08a4246651b7a9f06e0a97b04a068e6d7d8c9405
Jose Javier Mendoza Villalpando <Jose_Mendoza23@unikino.edu.mx>
2024-07-16 07:52:20 -0700

    copiando codigo desde listas simplemente ligadas para ser refactorizado.

--- ListasSimplementeLigadasCirculares/Program.cs
+++ ListasSimplementeLigadasCirculares/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("F***k Martes!");
+            Lista lista = new Lista();
         }
     }
 }
